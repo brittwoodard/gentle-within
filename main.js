2a2b62a2ddb5c6d100c0802000dd19d9c36aa950
brittwoodard <96916819+brittwoodard@users.noreply.github.com>
2025-02-26 15:53:49 +0100

    slick

---
+++ main.js
@@ -0,0 +1,12 @@
+// Slick
+$('.testimonial-container').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 500,
+    arrows: false,
+    accessibility: true,
+    dots: true,
+    fade: true,
+    infinite: true,
+    pauseOnHover: true,
+});
